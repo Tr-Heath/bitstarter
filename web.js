e836f12280af5d6a2f2887a22aa3327898e155ec
Ubuntu <ubuntu@ip-172-31-25-180.us-west-2.compute.internal>
2013-07-15 06:08:21 +0000

    Changed the response to print the buffer of index.html instead

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer();
+var buffer = new Buffer(128);
 buffer = fs.readFileSync("index.html");
 
 
