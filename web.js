4ee4c926253893df7f79a75078821607c888fe02
Ubuntu <ubuntu@ip-172-31-25-180.us-west-2.compute.internal>
2013-07-15 06:11:37 +0000

    Figuring out buffer

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = new Buffer(128);
-buffer = fs.readFileSync("index.html");
-
+//buffer = fs.readFileSync("index.html");
+buffer = "This is a test";
 
 
 app.get('/', function(request, response) {
