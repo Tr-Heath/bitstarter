3c87492683f39784bc5e38805a76afed26360b48
Ubuntu <ubuntu@ip-172-31-25-180.us-west-2.compute.internal>
2013-07-16 04:22:07 +0000

    one more buffer attempt

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var fs = requre('fs');
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -6,11 +6,12 @@ var app = express.createServer(express.logger());
 
 var buffer = new Buffer(128);
 //buffer = fs.readFileSync("index.html");
-//buffer = "This is a test";
+buffer = "This is a test";
 
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("./index.html").toString());
+  //response.send(fs.readFileSync("./index.html").toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
